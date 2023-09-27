08ded5e1db3e402c712123ee368f3debbf01510c
Kevin Augustin <kevinaugustin41@gmail.com>
2023-09-27 15:28:37 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require("express")
-const app = express()
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {});
 
-    
-})
-
-server.listen(3000)
+// cszc
+server.listen(3000);
